479324c1a2b95c2f81e518705978cc34c80a0b23
cjumelin <cjumelin@protonmail.com>
2023-07-28 18:40:52 -0400

    feat(polling): polling last 20 block and compute number of relayMsgPayment implemented

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,29 @@ import { relayCounterInLastNBlock } from "./useCases/relayCounterInLastNBlock/re
 import { retrieveLastBockHeight } from "./useCases/retrieveLastBlockHeight/retrieveLastBlockHeight";
 import { generateLavaClient } from "./utils/LavaClient";
 
-async function init() {
-    const lavaClient = await generateLavaClient({
-        chainID: 'LAV1',
-        rpcInterface: 'tendermintrpc'
-    })
-    const res = await relayCounterInLastNBlock(
-        lavaClient,
-        await retrieveLastBockHeight(lavaClient)
-    );
-    console.log(res.slice(0, 10))
+const pollingTime = 1000 * 60 * 5; // Every 5 minutes
+
+const poll = async (fn: () => Promise<any>) => {
+    await fn();
+    setTimeout(() => poll(fn), pollingTime);
 }
+
+async function init() {
+    try {
+        const lavaClient = await generateLavaClient({
+            chainID: 'LAV1',
+            rpcInterface: 'tendermintrpc'
+        });
+        poll(async () => {
+            const res = await relayCounterInLastNBlock(
+                lavaClient,
+                await retrieveLastBockHeight(lavaClient)
+            );
+            console.log(res.slice(0, 10)); // Replace by an event in the future
+        });
+    } catch (e) {
+        console.log(e)
+    }
+};
 
 init();
--- src/utils/LavaClient.ts
+++ src/utils/LavaClient.ts
@@ -4,16 +4,25 @@ import { Network } from "../types/network.type";
 //Initialize Lava SDK to the selected API
 export const generateLavaClient = async (
     { chainID, rpcInterface }: Network): Promise<LavaSDK> => {
-    return await LavaSDK.create({
-        privateKey: process.env.LAVA_PRIVKEY,
-        badge: {
-            badgeServerAddress: "https://badges.lavanet.xyz", // Or your own Badge-Server URL 
-            projectId: "3f850af078b30020705043f13970b901"
-        },
-        chainID,
-        rpcInterface, // Optional
-        geolocation: "2",
-    });
+    let lavaClient: LavaSDK;
+    try {
+        lavaClient = await LavaSDK.create({
+            privateKey: process.env.LAVA_PRIVKEY,
+            badge: {
+                badgeServerAddress: "https://badges.lavanet.xyz", // Or your own Badge-Server URL 
+                projectId: "3f850af078b30020705043f13970b901"
+            },
+            chainID,
+            rpcInterface, // Optional
+            geolocation: "2",
+        });
+    } catch (e: any) {
+        throw new Error(e)
+    }
+
+    if ((lavaClient as any).error)
+        throw new Error((lavaClient as any).error);
+    return lavaClient
 }
 
 type RPCCallArgs = {
@@ -23,7 +32,14 @@ type RPCCallArgs = {
 
 const RPCCall = (lavaClient: LavaSDK) =>
     async (rpcCallArgs: RPCCallArgs) => {
-        const res = await lavaClient.sendRelay(rpcCallArgs)
+        let res: any;
+        try {
+            res = await lavaClient.sendRelay(rpcCallArgs)
+        } catch (e: any) {
+            throw new Error(e);
+        }
+        if (res.error)
+            throw new Error(res.error);
         return JSON.parse(res);
     }
 
