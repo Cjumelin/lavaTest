7b39ebe178fa12e4bb4f66ede55a88109c07eaa1
cjumelin <cjumelin@protonmail.com>
2023-07-30 15:16:57 -0400

    feat(cli): cli polling block height implemented

--- src/cli.ts
+++ src/cli.ts
@@ -9,18 +9,30 @@ async function init() {
             chainID: 'LAV1',
             rpcInterface: 'tendermintrpc'
         });
+        let blocks: any[] = [];
+        let lastBlockHeight: number = -1;
         poll(async () => {
-            let res: any;
             try {
-                res = await getBlocks(lavaClient)(await retrieveLastBockHeight(lavaClient), 20)
+                const lastBlockHeightRes = await retrieveLastBockHeight(lavaClient);
+                if (lastBlockHeightRes !== lastBlockHeight) {
+                    lastBlockHeight = lastBlockHeightRes;
+                    const newBlocks = await getBlocks(lavaClient)(
+                        lastBlockHeightRes,
+                        blocks.length ? 1 : 20
+                    );
+                    blocks = [
+                        ...newBlocks.blocks, // retrieve the last block if blocks have already been retrieved
+                        ...blocks
+                    ].slice(0, 20)
+                    const relayCountPerChain = relayCounterInBlocks(
+                        blocks
+                    );
+                    console.log(relayCountPerChain.slice(0, 10));
+                }
             } catch (e) {
                 console.log(e)
             }
-            const relayCountPerChain = relayCounterInBlocks(
-                res.blocks
-            );
-            console.log(relayCountPerChain.slice(0, 10));
-        });
+        }, 1000);
     } catch (e) {
         console.log(e)
     }
